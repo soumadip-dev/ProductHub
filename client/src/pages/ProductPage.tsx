1d498ad2961741947bfd8bda1a2537cdd0138465
soumadip-dev <soumadipmajila@gmail.com>
2026-01-21 20:29:04 +0530

    feat: add React Query hooks for fetching a product by ID and deleting a product with mutation support

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,3 +1,23 @@
+import { useAuth } from '@clerk/clerk-react';
+import { Link, useNavigate, useParams } from 'react-router';
+import { useProduct } from '../hooks/useProducts';
+import { ArrowLeftIcon, EditIcon } from 'lucide-react';
+
 export default function ProductPage() {
-  return <div>Product</div>;
+  const { id } = useParams();
+  const { userId } = useAuth();
+  const navigate = useNavigate();
+
+  const { data: product, isLoading, error } = useProduct(id as string);
+
+  const isOwner = product?.userId === userId;
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <div className="flex items-center justify-between">
+        <Link to="/" className="btn btn-ghost btn-sm gap-1">
+          <ArrowLeftIcon className="size-4" /> Back
+        </Link>
+      </div>
+    </div>
+  );
 }
